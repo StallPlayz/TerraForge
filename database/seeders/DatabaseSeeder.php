cd4ec8fa8f1222e7ea5494ef6e826f52725bc991
StallPlayz <stallplayz123@gmail.com>
2026-08-07 16:03:05 +0700

    chore(db): add initial copper shortsword and blue slime seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,40 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Weapon;
+use App\Models\Enemy;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Weapon::create([
+            'name' => 'Copper Shortsword',
+            'base_damage' => 5,
+            'damage_type' => 'Melee',
+            'use_ammo_type' => null,
+            'projectile_count' => 1,
+            'base_crit_chance' => 4,
+            'armor_penetration' => 0,
+            'use_time' => 13,
+            'use_animation' => 13,
+            'auto_reuse' => false,
+            'mana_cost' => 0,
+            'melee_speed_application' => 'UseTime',
+            'is_true_melee' => true,
+            'reuse_delay' => 0,
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Enemy::create([
+            'name' => 'Blue Slime',
+            'base_defense' => 2,
+            'damage_reduction' => 0.0000, 
+            'debuff_immunities' => ['Poisoned'],
+            'bestiary_id' => 1,
         ]);
     }
 }
